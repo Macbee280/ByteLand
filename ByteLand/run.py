81b4802d3ee9d77b98b9a45c08f1079e070dbf97
gdimartino <gdimartino@gabedimartino.com>
2023-11-04 16:35:39 -0700

    Updated movement for turn

--- ByteLand/run.py
+++ ByteLand/run.py
@@ -6,11 +6,21 @@ Run project for AI Civilization
 
 from general import Character
 from general import *
+from Backend.map import *
+from Backend.navigation import *
+from Backend.utilities import *
 
 def main():
-    gabe = Character("GABE", "You are a villager in a small town of 4. You are new to this town and don't know many people. You are the new smith of the town.", "TOWNSQUARE")
+    game_map = GameMap(MAP_WIDTH, MAP_HEIGHT, TILE_SIZE)
+    collision_map = CollisionMap(game_map.map_data, COLLISION_CHAR)
+
+    gabe = Character("GABE", "You are a villager in a small town of 4. You are new to this town and don't know many people. You are the new smith of the town.", "TOWNSQUARE",coordinates=TOWN_CENTER)
     #izzy = Character("IZZY", "You are a villager in a small town of 4. You are the bartender of this town and heard there's a newcomer to the town. You want to meet him, his name is GABE.", "TOWNSQUARE")
     print(gabe.turn())
+    if gabe.turn()[0] == '[MOVE]':
+        gabe.location = gabe.turn()[1]
+        path = collision_map.find_path(gabe.coordinates, VARIABLE)
+        gabe.coordinates = VARIABLE
     #print(izzy.turn())
     
     # '[MOVE]',  'TAVERN'
