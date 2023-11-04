984d80757df23a786828ac18692c869e99fb9762
gdimartino <gdimartino@gabedimartino.com>
2023-11-04 10:44:19 -0700

    Fixed Coding Utils

--- ByteLand/general.py
+++ ByteLand/general.py
@@ -1,5 +1,7 @@
-""" gneral.py
+""" general.py
 
 Collection of functions to generate and run the AI civilization
 
 """
+import Coding_Utils.coding_utils as cu
+from Coding_Utils.coding_utils import err
