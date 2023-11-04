38a982a41ddbb8d431e7bf52030cd4f7a3b473be
Aiden Tabrah <115892124+IsaacSShade@users.noreply.github.com>
2023-11-04 11:25:12 -0700

    Basic Character Class

--- ByteLand/general.py
+++ ByteLand/general.py
@@ -5,3 +5,10 @@ Collection of functions to generate and run the AI civilization
 """
 import Coding_Utils.coding_utils as cu
 from Coding_Utils.coding_utils import err
+from Coding_Utils.object import Object
+
+# [USE] {SWORD}   [MOVE] {SMITHERY}
+
+class Character(Object):
+    def __init__():
+        pass
