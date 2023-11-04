655ef37dfe71da94dff34bf6ae7d04f55713c9a0
Aiden Tabrah <115892124+IsaacSShade@users.noreply.github.com>
2023-11-04 15:05:19 -0700

    Created a test main

--- ByteLand/run.py
+++ ByteLand/run.py
@@ -3,3 +3,13 @@
 Run project for AI Civilization
 
 """
+
+from general import Character
+
+def main():
+    gabe = Character("GABE", "You are a villager in a small town of 4. You are new to this town and don't know many people. You are the new smith of the town.", "TOWNSQUARE")
+    izzy = Character("IZZY", "You are a villager in a small town of 4. You are the bartender of this town and heard there's a newcomer to the town. You want to meet him, his name is GABE.", "TOWNSQUARE")
+    print(gabe.turn())
+    print(izzy.turn())
+    
+main()
