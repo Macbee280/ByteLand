2b78ec8a18b3696c7f408aab395e9fb17248d815
Aiden Tabrah <115892124+IsaacSShade@users.noreply.github.com>
2023-11-04 20:10:29 -0700

    AI Tweaks

--- ByteLand/run.py
+++ ByteLand/run.py
@@ -25,23 +25,29 @@ def find_people(prime_character):
                     
     return output
 
+def character_turn(person, map, options):
+    command, variable = person.turn(people=find_people(person))
+    run_command(person, command, variable, map, options, CHARACTERS=CHARACTERS)
+
 def main():
     opt = {}
     opt = configure_opt(opt)
     game_map = GameMap(opt['map_width'], opt['map_height'], opt['tile_size'])
     collision_map = CollisionMap(game_map.map_data, opt['collision_char'])
 
-    gabe = Character("GABE", "You are a villager named GABE in a small medieval town of 4. You are new to this town and don't know many people. You are the new smith of the town.", "TOWNSQUARE")
+    gabe = Character("GABE", "You are a villager named GABE in a small medieval town of 4. You are new to this town and don't know many people. You are the new smith of the town.", "TOWNSQUARE", coordinates=opt['coordinates']['TOWNSQUARE'])
     CHARACTERS[gabe.name] = gabe
-    izzy = Character("IZZY", "You are a villager named IZZY in a small medieval town of 4. You are the bartender of this town and heard there's a newcomer to the town. You want to meet him, his name is GABE.", "TOWNSQUARE")
+    izzy = Character("IZZY", "You are a villager named IZZY in a small medieval town of 4. You are the bartender of this town and heard there's a newcomer to the town. You want to meet him, his name is GABE.", "TOWNSQUARE", coordinates=opt['coordinates']['TOWNSQUARE'])
     CHARACTERS[izzy.name] = izzy
-    command, variable = gabe.turn(people=find_people(gabe))
-
-    if command == '[MOVE]':
-        run_command(gabe, command, variable, collision_map, opt)
-    else:
-        run_command(gabe, command, variable, CHARACTERS=CHARACTERS)
+    aiden = Character("AIDEN", "You are a villager named AIDEN in a small medieval town of 4. You are the town leader, a retired wizard and you want to make sure the jester, MILES isn't too mischievous.", "MARKET", coordinates=opt['coordinates']['MARKET'])
+    CHARACTERS[aiden.name] = aiden
+    miles = Character("MILES", "You are a villager named MILES in a small medieval town of 4. You are the jester of this town and heard there's a newcomer to the town. You want to meet him, and prank him.", "MARKET", coordinates=opt['coordinates']['MARKET'])
+    CHARACTERS[miles.name] = miles
     
-    #print(izzy.turn())
+    character_turn(gabe, collision_map, opt)
+    character_turn(izzy, collision_map, opt)
+    character_turn(aiden, collision_map, opt)
+    character_turn(miles, collision_map, opt)
+
 
 main()
