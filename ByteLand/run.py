a7afa37ddb3e5eb8abc99abdc65d52d7d7510c37
gdimartino <gdimartino@gabedimartino.com>
2023-11-04 18:20:19 -0700

    fixed opt

--- ByteLand/run.py
+++ ByteLand/run.py
@@ -16,7 +16,7 @@ def main():
     game_map = GameMap(opt['map_width'], opt['map_height'], opt['tile_size'])
     collision_map = CollisionMap(game_map.map_data, opt['collision_char'])
 
-    gabe = Character("GABE", "You are a villager in a small town of 4. You are new to this town and don't know many people. You are the new smith of the town.", "TOWNSQUARE",coordinates=TOWN_CENTER)
+    gabe = Character("GABE", "You are a villager in a small town of 4. You are new to this town and don't know many people. You are the new smith of the town.", "TOWNSQUARE",coordinates=opt['coordinates']['TOWNSQUARE'])
     CHARACTERS[gabe.name] = gabe
     izzy = Character("IZZY", "You are a villager in a small town of 4. You are the bartender of this town and heard there's a newcomer to the town. You want to meet him, his name is GABE.", "TOWNSQUARE")
     CHARACTERS[izzy.name] = izzy
